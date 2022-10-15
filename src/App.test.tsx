f1a076b8b92528ce77ada695a460d6a439119692
Pedro Gomez <pedro.gomez@elaniin.com>
2022-10-15 10:33:26 -0600

    🧪 add todo test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,4 +2,8 @@ import { describe } from 'vitest'
 
 describe('App', () => { 
   it.todo('should render');
+  it.todo('should have title "Mi DUI es valido?"');
+  it.todo('should have an input text');
+  it.todo('should have a button with text "Validar"');
+  /* Lo que demas que se les ocurra que puedan probar */
 })
