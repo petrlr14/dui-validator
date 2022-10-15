96eaaf897c79ab67d63d706865c03a387a6c997e
Pedro Gomez <pedro.gomez@elaniin.com>
2022-10-15 01:13:14 -0600

    ✨ add dui validator

---
+++ src/duiValidator.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect } from 'vitest'
+import duiValidator from './duiValidator'
+
+describe("duiValidator", () => { 
+  it('should return false if dui lenght !== 9', () => { 
+    expect(duiValidator("00000000")).toBe(false);
+  })
+  it("should return false on characters in dui", () => { 
+    expect(duiValidator("hola56789")).toBe(false);
+  })
+  it('should return false on invalid dui', () => { 
+    expect(duiValidator("123456789")).toBe(false);
+  })
+  it("should return true on valid dui", () => { 
+    expect(duiValidator("023827235")).toBe(true);
+  })
+})
