ad11b8d6c490663ce99ff7a581a7026605a4b2ad
Pedro Gomez <pedro.gomez@elaniin.com>
2022-10-15 01:13:35 -0600

    ✨ add dui validation to ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
+import duiValidator from './duiValidator'
 
 function App() {
 
   const [value, setValue] = useState("")
 
+  const onSubmit = () => { 
+    if (duiValidator(value)) {
+      console.log('es true');
+    } else { 
+      console.log('no es true');
+    }
+  }
+
   return (
-    <div className='bg-slate-300 w-full min-h-screen flex items-center justify-center flex-col'>
+    <div className='relative bg-slate-300 w-full min-h-screen flex items-center justify-center flex-col'>
       <div className='w-full max-w-lg text-center flex flex-col gap-16'>
         <h1 className='text-5xl font-bold'>Mi DUI es valido?</h1>
-        <form className='flex flex-col gap-5 items-center'>
+        <form
+          className='flex flex-col gap-5 items-center'
+          onSubmit={(e) => {
+            e.preventDefault()
+            onSubmit()
+           } }
+        >
           <input
             placeholder='000000000'
             className='rounded p-2 w-full'
